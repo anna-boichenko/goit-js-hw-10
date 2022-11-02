6b17c9b178b66cb67254484f7259e01913136b50
anna-boychenko <anna.boichenko2203@gmail.com>
2022-11-02 11:51:37 +0200

    add renderCountry

--- src/index.js
+++ src/index.js
@@ -12,62 +12,68 @@ const countryInfo = document.querySelector('.country-info');
 searchInput.addEventListener("input", debounce(searchCountry, DEBOUNCE_DELAY));
 
 function searchCountry(evt) {
-    const country = searchInput.value.trim();
+  evt.preventDefault();
+    const countryInp = searchInput.value.trim();
     // clearSearch();
-    if (!country) {
+    if (!countryInp) {
         return clearSearch();
-    }
+    };
 
-    fetchCountries(country)
-        .then(countries => {
+    fetchCountries(countryInp)
+        .then(country => {
           clearSearch();
-            if (countries.length > 10) {
-                Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
-            } else if(countries.length > 2 && countries.length < 10) {
-                return renderMarkupOfCountries(countries);
-            } else if (countries.length === 1) {
-                return renderCountry(countries);
+            if (country.length > 10) {
+              return Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+            } else if(country.length > 2 && country.length < 10) {
+                return renderMarkupOfCountries(country);
+            } else if (country.length === 1) {
+                return renderCountry(country);
             }
         }).catch(err => {
-            // clearSearch()
+          // clearSearch();
             return Notiflix.Notify.failure("Oops, there is no country with that name");
     })
 }
 
-function renderMarkupOfCountries(array) {
-    const markup = array.map(({ flags, name }) => {
-        return `
-        <li> 
+function renderMarkupOfCountries(country) {
+  console.log(country);
+  const markup = country
+    .map(({ flags, name }) => 
+      `<li> 
         <img src="${flags.svg}" alt="${name}" />
-        <h2>${name}</h2>`
-    }).join('');
-    countries.innerHTML = markup;
+        <h2>${name.official}</h2>
+        </li>`).join('');
+    countriesList.innerHTML = markup;
 }
+
+
+console.log(renderMarkupOfCountries());
 
 function renderCountry(array) {
     const markup = array.map(({ flags, name, capital, population, languages }) => {
-        `    <div class="country-card">
+      `<div class="country-card">
       <img class="country-card__img" src="${flags.svg}" alt="" />
-      <p class="country-card__name">${name}</p>
+      <p class="country-card__name">${name.official}</p>
+      </div>
       <div>
-        <p class='text'>Capital:
-          <span class='span'>${capital}</span>
+        <p class="text">Capital:
+          <span class="span">${capital}</span>
         </p>
       </div>
       <div>
-        <p class='text'>Population:
-          <span class='span'>${population}</span>
+        <p class="text">Population:
+          <span class="span">${population}</span>
         </p>
       </div>
       <div>
-        <p class='text'>Languages:
-          <span class='span'>${languages}</span>
+        <p class="text">Languages:
+          <span class="span">${Object.values(languages)}</span>
         </p>
-      </div>
-    </div>`
-    });
+      </div>`
+    }).join("");
     countryInfo.innerHTML = markup;
 }
+
 function clearSearch() {
     countriesList.innerHTML = "";
     countryInfo.innerHTML = "";
